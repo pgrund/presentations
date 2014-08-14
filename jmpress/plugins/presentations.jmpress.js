9d67d88a4df0893b9793892ec9eaa301df969f35
Peter Grund <peter.grund@bisnode.de>
2014-08-14 07:57:21 +0200

    added custom plugin (break, print, footer update, ...)

---
+++ jmpress/plugins/presentations.jmpress.js
@@ -0,0 +1,103 @@
+/*!
+ * plugin for jmpress.js v0.4.5
+ *
+ * Copyright 2014 Peter Grund
+ * Licensed MIT
+ * http://www.opensource.org/licenses/mit-license.php
+ */
+/*
+ * jmpress custom plugin
+ * custom functionality of all presentations by Peter
+ */
+(function($, document, window, undefined) {
+    'use strict';
+
+    /* DEFAULTS */
+	$.jmpress("defaults").presentations = {
+		print: {
+            button: "#printbutton"
+        },
+        break: {
+            button: "#breakbutton",
+            modal: "#break",
+            counter: "#counter"
+        },
+        date: {
+            meta: "meta[name=date]"
+        }
+	};
+
+    function getElementID(elementID, defaultValue) {
+       elementID = (elementID != null && elementID != "") ? elementID : defaultValue;
+       elementID = (elementID.indexOf("#") == 0) ? elementID : "#" + elementID;
+       if ($(elementID).length == 0) {
+            console.error("no element found for " + elementID + " !!!");
+            return false;
+       }
+       return elementID
+    }
+
+    $.jmpress("register", "print", function(config, eleID) {
+        var jmpress = this;
+        
+        var elementID = getElementID(eleID, $(jmpress).jmpress("settings").presentations.print.button);
+       
+        if ($(elementID)) {
+            $(elementID).click(function() {
+                if ($(jmpress).jmpress("initialized")) {
+                    $(jmpress).jmpress("deinit");
+                    window.print();
+                    $(jmpress).jmpress(config);
+                } else {
+                    window.print();
+                }
+
+            });
+        }
+    });
+    
+    $.jmpress("register", "break", function(config, eleID) {
+        var jmpress = this;
+
+        var elementID = getElementID(eleID, $(jmpress).jmpress("settings").presentations.break.button);
+        console.log(elementID);
+
+        if ($(elementID)) {
+            $(elementID).click(function() {
+                var c = $("#counter");
+                var b = $("#break");
+                c.html("");
+                c.countdown({
+                    step: 5,
+                    format: 'mm:ss',
+                    startTime: '05:00',
+                    timerEnd: function() {
+                        b.modal("hide");
+                    },
+                    image: "../bisnode/img/digits.png"
+                });
+                b.modal("show");            
+            });
+        }else{
+            console.error("no element found for " + elementID + " !!!");
+        }
+    });
+
+    $.jmpress("register" , "dateFooter", function(eleID){
+        var jmpress = this;
+
+        var d = new Date();
+        var m = $($(jmpress).jmpress("settings").presentations.date.meta);
+        
+        var footerDate = d.getFullYear() + "-"
+                + ((d.getMonth() + 1) < 10 ? "0" + (d.getMonth() + 1) : d.getMonth()) + "-"
+                + ((d.getDate() < 10) ? "0" + d.getDate() : d.getDate());
+
+        if ( m && m.attr("content")) {
+            footerDate = m.attr("content");
+        }
+        
+        $("footer").prepend("       " + footerDate + "          ");
+    });
+
+}(jQuery, document, window));
