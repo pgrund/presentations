dc32e4cffc84833bf082edd4f0e3952bffe54fb1
Peter Grund <peter.grund@bisnode.de>
2014-10-23 14:28:27 +0200

    usinh pre 5.3 version of php

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 <p>The following presentations are currently available:</p>
 <ul>
 <?php
-foreach (glob("*/index.html") as $filename) {    
-    $folder = strstr($filename, '/', true);
+foreach (glob("*/index.html") as $filename) {
+    $folder = substr($filename, 0,strpos($filename, "/"));
 ?>
   <li>
     <a href="./<?=$filename?>"><?=$folder?></a>
